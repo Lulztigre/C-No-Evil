429e6dd36271995b9a3ede6b5926b18a15049c2a
Lulztigre <37852930+Lulztigre@users.noreply.github.com>
2024-01-30 09:32:00 +0000

    Create Exercise_1-3.c

---
+++ Exercise_1-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int fahr;
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+    
+    printf("FAHRENHEIT\tCELSIUS\n");
+    for (fahr = LOWER; fahr <= 300; fahr = fahr + STEP)
+    {
+        printf("%3.0f\t\t %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
+    }
+    return 0;
+}
